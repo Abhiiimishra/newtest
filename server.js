c5ef1b35a5c80c0423b324631cd8832107726e26
Abhishek Mishra <abhishek.mishra@aamokshoneeighty.com>
2024-06-21 03:33:23 -0700

    changes

--- server.js
+++ server.js
@@ -1,38 +1,11 @@
 const express = require('express');
-const ngExpressEngine = require('@nguniversal/express-engine').ngExpressEngine;
-const { provideModuleMap } = require('@nguniversal/module-map-ngfactory-loader');
-const { join } = require('path');
-const { enableProdMode } = require('@angular/core');
-
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/testapp/server/main');
-
-enableProdMode();
-
 const app = express();
-const PORT = process.env.PORT || 8080;
-const DIST_FOLDER = join(process.cwd(), 'dist/testapp/browser');
+const path = require('path');
 
-// Set the view engine
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
+app.use(express.static(__dirname + '/dist/testapp'));
 
-app.set('view engine', 'html');
-app.set('views', DIST_FOLDER);
-
-// Serve static files
-app.get('*.*', express.static(DIST_FOLDER, {
-  maxAge: '1y'
-}));
-
-// All regular routes use the Universal engine
-app.get('*', (req, res) => {
-  res.render('index', { req });
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/testapp/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Node Express server listening on http://localhost:${PORT}`);
-});
+app.listen(process.env.PORT || 8080);
